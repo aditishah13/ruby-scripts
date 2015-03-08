432ac8ee581a3cf0eed6229306141917eeb9954d
Aditi Shah <avakharia@gmail.com>
2015-03-08 05:26:33 -0400

    upload array.rb

---
+++ array.rb
@@ -0,0 +1,7 @@
+a = [ 45, 3, 19, 8 ]
+b = [ 'sam', 'max', 56, 98.9, 3, 10, 'jill' ]
+print (a + b).join(' '), "\n"
+print a[2], " ", b[4], " ", b[-2], "\n"
+print a.sort.join(' '), "\n"
+a << 57 << 9 << 'phil'
+print "A: ", a.join(' '), "\n"
